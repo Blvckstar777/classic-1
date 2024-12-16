34e80a8321bbd0c88d199030fa86345d8ea2ce8c
hcon241@gmail.com <hcon241@gmail.com>
2024-12-16 13:33:19 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,18 +1,18 @@
 import anvil.server
 import pandas as pd
-url1 = "https://github.com/Blvckstar777/classic-1/blob/master/AppleStore.csv"
+#url1 = "https://github.com/Blvckstar777/classic-1/blob/master/AppleStore.csv"
 #url1 = "https://www.kaggle.com/datasets/ysf12ff/app-store-dataset/data"
 #appstore_df = pd.read_html(url1)
-appstore_df = {'url':url1,
-              'name': 'appstore_data',
-              'load_func': lambda URL: pd.read_html(URL)[0]}
-
-df1 = appstore_df['load_func'](appstore_df['url'])
-#data = {
-#  "calories": [420, 380, 390],
-#  "duration": [50, 40, 45]
-#}
-#appstore_df = pd.DataFrame(data)
+#appstore_df = {'url':url1,
+#              'name': 'appstore_data',
+#              'load_func': lambda URL: pd.read_html(URL)[0]}
+#
+#df1 = appstore_df['load_func'](appstore_df['url'])
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+appstore_df = pd.DataFrame(data)
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -26,7 +26,7 @@ df1 = appstore_df['load_func'](appstore_df['url'])
 #   return 42
 @anvil.server.callable
 def explore():
-  print(appstore_df)
-  type(df1)
+  print(appstore_df.header())
+  #type(df1)
    
 
