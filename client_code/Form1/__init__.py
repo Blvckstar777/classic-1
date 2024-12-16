e5c97125956d44153de39deeddee9dbafb773ea9
hcon241@gmail.com <hcon241@gmail.com>
2024-12-16 14:13:05 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,4 +10,14 @@ class Form1(Form1Template):
     self.init_components(**properties)
     print('Hi')
     # Any code you write here will run before the form opens.
-    anvil.server.call('say_hello')
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('explore')
+    pass
+   
+
+  
+    
+
+  
