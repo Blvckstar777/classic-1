0314bc3875a865aa1cc95b06e185092a7fa9209c
hcon241@gmail.com <hcon241@gmail.com>
2024-12-27 12:26:13 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,6 +10,9 @@ class Form1(Form1Template):
     self.init_components(**properties)
     print('Hi')
     # Any code you write here will run before the form opens.
+    media_obj = anvil.server.call('get_plot1')
+    self.image_1.source = media_obj
+    print(type(media_obj))
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
